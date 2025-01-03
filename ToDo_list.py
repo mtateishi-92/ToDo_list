143b575ba5756daad3d73d28eb1fce3872bcc323
mtateishi-92 <stand.m.9287@gmail.com>
2025-01-03 15:30:44 +0900

    changed variables and comments

--- ToDo_list.py
+++ ToDo_list.py
@@ -1,10 +1,6 @@
-
-#空のリストを作成
-ToDo_List = []
-#入力を受け取る
-ToDo_disc = str(input('ToDoを入力してください。:'))
-#リストに入力を追加
-ToDo_List.append(ToDo_disc)
+todo_list = []
+todo_disc = str(input('ToDoを入力してください。:'))
+todo_list.append(todo_disc)
 
 #さらに入力を追加するか判定
 judge = str(input('他のToDoを入力しますか? yes or no :'))
@@ -16,9 +12,8 @@ while not judge == 'yes' and not judge == 'no':
 #yesの場合は追加を繰り返す
 while judge == 'yes' :
     if judge == 'yes':
-        ToDo_disc = str(input('ToDoを入力してください。:'))
-        #リストにToDoを追加
-        ToDo_List.append(ToDo_disc)
+        todo_disc = str(input('ToDoを入力してください。:'))
+        todo_list.append(todo_disc)
         #さらに入力を追加するか判定
         judge = str(input('他のToDoを入力しますか? yes or no :'))
         #yesまたはnoを入力するまで繰り返す
@@ -30,6 +25,6 @@ if judge == 'no':
     print('ToDoを表示します。')
 
 #ToDoを出力
-for number, ToDo in enumerate(ToDo_List, 1):
+for number, ToDo in enumerate(todo_list, 1):
     print(str(number) + ':' + ToDo)
 
